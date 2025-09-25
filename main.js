bf0535069cb4d68086479f99f42fcba67730752f
gloria-byte-mc <gloriamachoka1@gmail.com>
2025-09-25 15:05:52 +0300

    another-page

---
+++ main.js
@@ -0,0 +1,17 @@
+function function1(){
+  const log = document.querySelector(".log-in");
+
+  log.addEventListener("click", ()=>{
+    window.location.href = "log-in.html";
+  });
+}
+
+function function2(){
+const sign = document.querySelector(".sign-up");
+
+sign.addEventListener("click", ()=>{
+  window.location.href = "sign-up.html";
+});
+
+}
+
